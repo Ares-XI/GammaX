a2f30b0e5d9e72ccb16f55dc9e1b124b128f66d4
Egor <kasatkinegor2009@gmail.com>
2026-03-04 21:23:56 +0300

    now beta

--- modules/internal/src/main/java/io/gammax/internal/MixinStart.java
+++ modules/internal/src/main/java/io/gammax/internal/MixinStart.java
@@ -5,7 +5,7 @@ import java.lang.instrument.Instrumentation;
 public class MixinStart {
     public static void premain(String agentArgs, Instrumentation inst) {
         System.out.println("=====================================");
-        System.out.println("|| GammaX started! Version: 1.0 alpha");
+        System.out.println("|| GammaX started! Version: 1.0 beta");
         System.out.println("=====================================");
 
         MixinRegistry.loadCache();
